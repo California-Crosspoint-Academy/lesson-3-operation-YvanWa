06eef64c02dfc65c1968e49c215843a5910a0e83
YvanWa <yvanwang26@crosspointacademy.org>
2025-08-25 21:10:48 +0000

    commit

---
+++ SimpleOp.java
@@ -0,0 +1,31 @@
+//Yvan Wang
+public class SimpleOp{
+    public static void main(String[]args){
+      
+      //exercise 7
+      int count;
+      String a = "Look here!";
+      count = a.length();
+      System.out.println("\""+a+"\""+" has "+count+" characters.");
+    
+    //exercise 8
+    System.out.println("All \"good\" men should come to the aid of their country.");
+    
+    //exercise 9
+    System.out.println("Hello\nHello again");
+    
+    //exercise 10
+    System.out.println("A backlash looks like this \\, ...right?");
+
+    //excercise 11
+    String pq = "Eddie Haskel";
+    int hm = pq.length();
+    String ed=pq.substring(hm -4);
+    System.out.println(ed);
+    //answer before running the code: skel(hm=12-4=8)
+
+    //exercise 12 n
+
+
+    }
+}
