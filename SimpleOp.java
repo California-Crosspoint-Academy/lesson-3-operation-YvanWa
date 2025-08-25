b175f14de4aad3a05fb1e63795c111c8a2d40512
YvanWa <yvanwang26@crosspointacademy.org>
2025-08-25 23:10:49 +0000

    Final

--- SimpleOp.java
+++ SimpleOp.java
@@ -1,6 +1,14 @@
+//Yvan Wang
 public class SimpleOp{
     public static void main(String[]args){
         String nation="people's republic of China";
-        
+        System.out.println(nation.toUpperCase());
+        double age = 75.9;
+        String k = " is " + age + " years old.";
+        String name = nation.substring(21);
+        System.out.println(name + k );
+        System.out.println("It has a name called "+ name.toLowerCase()+" and it is really famous among the world.");
+
+
     }
 }
